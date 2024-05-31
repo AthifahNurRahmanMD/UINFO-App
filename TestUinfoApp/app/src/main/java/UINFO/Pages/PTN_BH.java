e621554c62ad9767d7577bf1b6ce96de87d9db14
AthifahNurRahmanMD <athifaahnur145@gmail.com>
2024-05-31 20:15:39 +0800

    feat: menambahkan scene untuk ptn bh

--- TestUinfoApp/app/src/main/java/UINFO/Pages/PTN_BH.java
+++ TestUinfoApp/app/src/main/java/UINFO/Pages/PTN_BH.java
@@ -25,11 +25,14 @@ public class PTN_BH {
 
     public void show(){
         Label ptnBh = new Label("PTN-BH");
-        ptnBh.getStyleClass().add("text-aboutptnbh1");
-        Text text = new Text("PTN-BH");
-        text.getStyleClass().add("PTN-BH");
+        ptnBh.getStyleClass().add("label-ptnbhstatushl");
+        // Text text = new Text("PTN-BH"); //yg ditengah
+        // text.getStyleClass().add("text-aboutptnbh1"); //bold
         Text text2 =  new Text("Perguruan Tinggi Berbadan Hukum");
         text2.getStyleClass().add("text-aboutptnbh2");
+        Text text3 = new Text("PTN BH adalah perguruan tinggi negeri yang didirikan oleh pemerintah dengan status\nberbadan hukum yang otonom. Yang artinya perguruan tinggi negeri tersebut\noleh pemerintah melalui Kemendikbud sudah diberi hak otonom agar lebih mandiri.");
+        text3.getStyleClass().add("text-abaoutptnbh3");
+
 
         Button backButton = new Button("Kembali");
         backButton.getStyleClass().add("button-backbutton");
@@ -39,7 +42,7 @@ public class PTN_BH {
                 ptnScene.show();
             });
         
-        Image backgroundImage = new Image(getClass().getResourceAsStream("/Images/newptnStatusbg.png"));
+        Image backgroundImage = new Image(getClass().getResourceAsStream("/Images/newptnstatusbg.png"));
 
         BackgroundImage backgroundImg = new BackgroundImage(backgroundImage,
             BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT, null,
@@ -47,11 +50,12 @@ public class PTN_BH {
 
         StackPane stackPane = new StackPane();
         StackPane.setMargin(ptnBh, new Insets(20,595,480,20));
-        StackPane.setMargin(text, new Insets(50,90,100,200));
-        StackPane.setMargin(text2, new Insets(80,170,50,30));
+        // StackPane.setMargin(text, new Insets(52,350,437,350));
+        StackPane.setMargin(text2, new Insets(58,350,435,350));
+        StackPane.setMargin(text3, new Insets(63,350,363,350));
         StackPane.setMargin(backButton, new Insets(430, 350, 15, 350));
         stackPane.setBackground(new Background(backgroundImg));
-        stackPane.getChildren().addAll(ptnBh,text,text2,backButton);
+        stackPane.getChildren().addAll(ptnBh,text2,text3,backButton);
         Scene scene = new Scene(stackPane, 800, 500);
         scene.getStylesheets().add(getClass().getResource("/Style/styles.css").toExternalForm());
         stage.setScene(scene);
